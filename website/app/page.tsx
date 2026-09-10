933e57d80adeceaf0b4505b3043c6bc866328bbf
tonykastaneda <tonykastaneda@gmail.com>
2026-09-09 20:40:04 -0700

    Remove today from homepage payment message

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -90,7 +90,7 @@ export default function Home() {
           <h2>Open to All.<br /><em>Yours to shape.</em></h2>
           <p className="section-intro">Amalith is free today. We plan to charge in the future, but no date or pricing has been announced.</p>
           <div className="principles__grid">
-            <p>No payment required today</p><p>No mandatory account</p><p>No proprietary cloud</p>
+            <p>No payment required</p><p>No mandatory account</p><p>No proprietary cloud</p>
             <p>Open document format</p><p>macOS, Windows &amp; Linux</p><p>MIT or Apache 2.0</p>
           </div>
         </section>
